fd12ff5acb6a7290845c8fde99763a319941cdb5
JorgeZuniga <alejandrojorge927@gmail.com>
2020-10-30 19:16:47 -0600

    perdon enconre otra clase que daba error y no la use perdon mi companero es el que hizo esas clases y no pudo terminarlas y solo me las envio con errores o imcompletas y ahorita solo la comente

--- Lab#3_JorgeZuniga_KevinRodriguez/src/lab/pkg3_jorgezuniga_kevinrodriguez/Restaurantes.java
+++ Lab#3_JorgeZuniga_KevinRodriguez/src/lab/pkg3_jorgezuniga_kevinrodriguez/Restaurantes.java
@@ -1,50 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package lab.pkg3_jorgezuniga_kevinrodriguez;
-
-import java.util.ArrayList;
-
-/**
- *
- * @author Kevin
- */
-public class Restaurantes extends Locales {
-    private ArrayList <Comida> menu;
-    private Comida platodia;
-    
-
-   
-    public Restaurantes(ArrayList<Comida> menu, Comida platodia, String nombreLoc, String empleado, ArrayList<Productos> productos, String gerente) {
-        super(nombreLoc, empleado, productos, gerente);
-        this.menu = menu;
-        this.platodia = platodia;
-    }
-    
-    //Accesores y mutadores
-
-    public ArrayList<Comida> getMenu() {
-        return menu;
-    }
-
-    public void setMenu(ArrayList<Comida> menu) {
-        this.menu = menu;
-    }
-
-    public Comida getPlatodia() {
-        return platodia;
-    }
-
-    public void setPlatodia(Comida platodia) {
-        this.platodia = platodia;
-    }
-    
-    //Administrativos
-
-    @Override
-    public String toString() {
-        return "Restaurantes{" + "menu=" + menu + ", platodia=" + platodia + '}';
-    }
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package lab.pkg3_jorgezuniga_kevinrodriguez;
+//
+//import java.util.ArrayList;
+//
+///**
+// *
+// * @author Kevin
+// */
+//public class Restaurantes extends Locales {
+//    private ArrayList <Comida> menu;
+//    private Comida platodia;
+//    
+//
+//   
+//    public Restaurantes(ArrayList<Comida> menu, Comida platodia, String nombreLoc, String empleado, ArrayList<Productos> productos, String gerente) {
+//        super(nombreLoc, empleado, productos, gerente);
+//        this.menu = menu;
+//        this.platodia = platodia;
+//    }
+//    
+//    //Accesores y mutadores
+//
+//    public ArrayList<Comida> getMenu() {
+//        return menu;
+//    }
+//
+//    public void setMenu(ArrayList<Comida> menu) {
+//        this.menu = menu;
+//    }
+//
+//    public Comida getPlatodia() {
+//        return platodia;
+//    }
+//
+//    public void setPlatodia(Comida platodia) {
+//        this.platodia = platodia;
+//    }
+//    
+//    //Administrativos
+//
+//    @Override
+//    public String toString() {
+//        return "Restaurantes{" + "menu=" + menu + ", platodia=" + platodia + '}';
+//    }
+//}
