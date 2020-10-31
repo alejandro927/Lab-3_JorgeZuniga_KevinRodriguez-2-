40d14711c18415a843ef9d6de31d43afbe966dfc
JorgeZuniga <alejandrojorge927@gmail.com>
2020-10-30 18:31:08 -0600

    agregue varias opciones en modificar para el administrador

--- Lab#3_JorgeZuniga_KevinRodriguez/src/lab/pkg3_jorgezuniga_kevinrodriguez/Lab3_JorgeZuniga_KevinRodriguez.java
+++ Lab#3_JorgeZuniga_KevinRodriguez/src/lab/pkg3_jorgezuniga_kevinrodriguez/Lab3_JorgeZuniga_KevinRodriguez.java
@@ -74,8 +74,8 @@ public class Lab3_JorgeZuniga_KevinRodriguez {
                                         opcion1 = sc.nextInt();
                                         switch (opcion1) {
                                             case 1: {
-                                                int posi, posi1,precio;
-                                                String nombre, nombregenerente = "", nombreemp,nombproducto,descrip;
+                                                int posi, posi1, precio;
+                                                String nombre, nombregenerente = "", nombreemp, nombproducto, descrip;
                                                 System.out.println("Ingrese el nombre de la tienda:");
                                                 nombre = sc.nextLine();
                                                 nombre = sc.nextLine();
@@ -96,7 +96,6 @@ public class Lab3_JorgeZuniga_KevinRodriguez {
                                                 if ("".equals(nombregenerente)) {
                                                     nombregenerente = ((Personas) empleado.get(posi1)).getNombre();
                                                 }
-                                                nombreemp = ((Personas) empleado.get(posi1)).getNombre();
                                                 System.out.println("Ingrese el nombre del producto:");
                                                 nombproducto = sc.nextLine();
                                                 nombproducto = sc.nextLine();
@@ -106,7 +105,7 @@ public class Lab3_JorgeZuniga_KevinRodriguez {
                                                 System.out.println("Ingrese el precio del producto:");
                                                 precio = sc.nextInt();
                                                 productos.add(new Productos(salida, nombre, precio));
-                                                tiendas.add(new Locales(nombre, empleado, id, nombregenerente));
+                                                tiendas.add(new Locales(nombre, empleado, productos, nombregenerente));
                                                 empleado.remove(posi);
                                                 String salida1 = "";
                                                 System.out.println("Su lista de tiendas es:");
@@ -117,19 +116,20 @@ public class Lab3_JorgeZuniga_KevinRodriguez {
                                             }
                                             break;
                                             case 2: {
-                                                String salida1="";
-                                                System.out.println("Su lista de tiendas es:");
-                                                for (Object t : tiendas) {
-                                                    salida1 += "" + tiendas.indexOf(t) + "=> " + t + "\n";
-                                                }
-                                                System.out.println(salida1);
-                                                int op1,posi;
-                                                System.out.println("Ingrese la posicion que desea modificar:");
-                                                posi = sc.nextInt();
-                                                System.out.println("Que desea modificar?:\n"
-                                                        + "1)Nombre del local\n"
-                                                        + "2)gerente\n"
-                                                        + "");
+//                                                String salida1 = "";
+//                                                System.out.println("Su lista de tiendas es:");
+//                                                for (Object t : tiendas) {
+//                                                    salida1 += "" + tiendas.indexOf(t) + "=> " + t + "\n";
+//                                                }
+//                                                System.out.println(salida1);
+//                                                int op1;
+//                                                System.out.println("Elija su opcion que desea tener un quiosquito:");
+//                                                op = sc.nextInt();
+//                                                while(op<0 || op > tiendas.size()){
+//                                                    System.out.println("La opcion marcada no existe ingrese una nueva:");
+//                                                    op = sc.nextInt();
+//                                                }
+//                                                System.out.println("Ingrese");
                                             }
                                             break;
                                         }
@@ -137,17 +137,49 @@ public class Lab3_JorgeZuniga_KevinRodriguez {
                                     }
                                     break;
                                     case 2: {
-                                        String nombre;
-                                        System.out.println("Ingrese el nombre de la tienda del quiosco:");
-                                        nombre = sc.nextLine();
-                                        nombre = sc.nextLine();
-                                        if (nombreLocales.contains(nombre)) {
-                                            System.out.println("La ID ya existe ingrese nuevamente:");
-                                            nombre = sc.nextLine();
-                                            quioscos.add(nombre);
-                                        } else {
-                                            System.out.println("La tienda que escribio no existe");
-                                            break;
+                                        String salida1 = "";
+                                        System.out.println("Su lista de tiendas es:");
+                                        for (Object t : tiendas) {
+                                            salida1 += "" + tiendas.indexOf(t) + "=> " + t + "\n";
+                                        }
+                                        System.out.println(salida1);
+                                        int op1, posi;
+                                        String nombrelocal;
+                                        System.out.println("Ingrese la posicion que desea modificar:");
+                                        posi = sc.nextInt();
+                                        System.out.println("Que desea modificar?:\n"
+                                                + "1)Nombre del local\n"
+                                                + "2)Gerente\n"
+                                                + "3)Producto\n"
+                                                + "4)Empleado\n"
+                                                + "Ingrese su opcion:");
+                                        op1 = sc.nextInt();
+                                        switch(op1){
+                                            case 1:{
+                                                System.out.println("Ingrese el nuevo nombre del local:"); 
+                                                nombrelocal = sc.nextLine();
+                                                nombrelocal = sc.nextLine();
+                                                nombreLocales.set(posi, nombrelocal);
+                                                ((Locales)tiendas.get(posi)).setNombre(nombrelocal);
+                                                
+                                            }break;
+                                            case 2:{
+                                                String salida = "",gerentex="";
+                                                int posix;
+                                                for (Object t : empleado) {
+                                                    salida += "" + empleado.indexOf(t) + "=> " + t + "\n";
+                                                }
+                                                System.out.println("Ingrese la posicion del empleado que quiere para gerente:");
+                                                posix = sc.nextInt();
+                                                gerentex = ((Personas)empleado.get(posix)).getNombre();
+                                                ((Locales)tiendas.get(posi)).setNombre(gerentex);
+                                            }break;
+                                            case 3:{
+                                                
+                                            }break;
+                                            case 4:{
+                                                
+                                            }break;
                                         }
 
                                     }
