395ae22ab6b0d8132877e49fbd9bfc5b5c8b557c
kevin09102001 <kevin31344@unitec.edu>
2020-10-30 18:17:45 -0600

    Clase restaurante hecha

--- Lab#3_JorgeZuniga_KevinRodriguez/src/lab/pkg3_jorgezuniga_kevinrodriguez/Restaurantes.java
+++ Lab#3_JorgeZuniga_KevinRodriguez/src/lab/pkg3_jorgezuniga_kevinrodriguez/Restaurantes.java
@@ -5,10 +5,46 @@
  */
 package lab.pkg3_jorgezuniga_kevinrodriguez;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Kevin
  */
-public class Restaurantes {
+public class Restaurantes extends Locales {
+    private ArrayList <Comida> menu;
+    private Comida platodia;
     
+
+   
+    public Restaurantes(ArrayList<Comida> menu, Comida platodia, String nombreLoc, String empleado, ArrayList<Productos> productos, String gerente) {
+        super(nombreLoc, empleado, productos, gerente);
+        this.menu = menu;
+        this.platodia = platodia;
+    }
+    
+    //Accesores y mutadores
+
+    public ArrayList<Comida> getMenu() {
+        return menu;
+    }
+
+    public void setMenu(ArrayList<Comida> menu) {
+        this.menu = menu;
+    }
+
+    public Comida getPlatodia() {
+        return platodia;
+    }
+
+    public void setPlatodia(Comida platodia) {
+        this.platodia = platodia;
+    }
+    
+    //Administrativos
+
+    @Override
+    public String toString() {
+        return "Restaurantes{" + "menu=" + menu + ", platodia=" + platodia + '}';
+    }
 }
